759e5acf98454bb6c0a32b20461a2e425b7ec1cd
Ericson Luciano <design.ericson@gmail.com>
2014-08-14 14:43:02 +0800

    add photo to scaffold

--- app/models/photogallery.rb
+++ app/models/photogallery.rb
@@ -1,2 +1,5 @@
 class Photogallery < ActiveRecord::Base
+
+  has_attached_file :photo, :styles => { :large => "800x800",:medium => "500x500", :small => "250x250" }
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
